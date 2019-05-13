47b451b235990c8222158d9f69384b90fc0bd2be
Anom Suroto <50583905+surotolinggau@users.noreply.github.com>
2019-05-13 21:33:10 +0700

    linggau

---
+++ buka-tombol.js
@@ -0,0 +1,13 @@
+// Demo by http://creative-punch.net
+
+var items = document.querySelectorAll('.bulat a');
+
+for(var i = 0, l = items.length; i < l; i++) {
+  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+  
+  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+}
+
+document.querySelector('.tombol').onclick = function(e) {
+   e.preventDefault(); document.querySelector('.bulat').classList.toggle('buka');
+}
